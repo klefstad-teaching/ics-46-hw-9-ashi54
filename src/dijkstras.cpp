0284e7413a089a206916a7b5aadbea6a5dd68b96
ashi54 <akshines54@gmail.com>
2025-03-17 06:34:43 +0000

    Fixed the print path method

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -10,33 +10,41 @@ const int UNDEFINED = -1;
 
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-    int numVertices = G.size(); // Get the number of vertices in the graph
-    vector<int> distances(numVertices, INF); // Initialize distances to INF
-    vector<bool> visited(numVertices, false); // Track visited vertices
-    previous.assign(numVertices, UNDEFINED); // Initialize previous vector
+    /*
+    (0) --5--> (1)
+     |        /
+     2      3
+     |    /
+    (2)
+    */
 
-    // Min-heap priority queue (pair: {distance, vertex})
+    int numVertices = G.size(); 
+    // initialize to infinity
+    vector<int> distances(numVertices, INF); 
+    vector<bool> visited(numVertices, false);
+    previous.assign(numVertices, UNDEFINED);
+
+    // want minheap so that the shortest is added everytime!!
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
     
-    // Start with the source node
     distances[source] = 0;
     previous[source] = UNDEFINED;
     minHeap.push({0, source});
 
-    // Process nodes in order of shortest distance
     while (!minHeap.empty()) {
-        int u = minHeap.top().second; // Extract vertex with smallest distance
+        // get the node with the shortest distance
+        int u = minHeap.top().second;
         minHeap.pop();
 
-        if (visited[u]) continue; // Skip already visited nodes
+        if (visited[u]) continue; 
         visited[u] = true;
-
-        // Explore all edges from vertex u
+        // exploring all the edges from the node you are looking at
         for (const Edge& edge : G[u]) {
             int v = edge.dst;
             int weight = edge.weight;
 
-            // If a shorter path to v is found, update distance and path
+            // if the shortest is fo0und, need to update the distance and path
+            // would be (distance[u] + weight)
             if (!visited[v] && distances[u] + weight < distances[v]) {
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
@@ -45,92 +53,24 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
         }
     }
 
-    return distances; // Return the shortest distances from the source
+    return distances;
 }
-
-
-
-
-
-// vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-//     /*
-//     (0) --5--> (1)
-//      |        /
-//      2      3
-//      |    /
-//     (2)
-
-//     source = 0
-//     distance = [0,5, 2]
-//     previous = [-1, 0, 0]
-//     */
-//     int numVertices = G.size();
-//     // initialize to infinity
-//     vector<int> distances(numVertices, INF);
-//     vector<bool> visited(numVertices, false);
-//     previous.assign(numVertices, -1);  
-
-//     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
-    
-//     distances[source] = 0;
-//     // want minheap so that the shortest is added everytime!!
-//     minHeap.push({0, source});
-
-//     while (!minHeap.empty()) {
-//         // get the node with the shortest distance
-//         int u = minHeap.top().second;
-//         minHeap.pop();
-
-//         if (visited[u]) continue;
-//         visited[u] = true;
-//         // exploring all the edges from the node you are looking at
-//         for (const Edge& edge : G[u]) {
-//             int v = edge.dst;
-//             int weight = edge.weight;
-
-//             // if the shortest is fo0und, need to update the distance and path
-//             // would be (distance[u] + weight)
-//             if (!visited[v] && distances[u] + weight < distances[v]) {
-//                 distances[v] = distances[u] + weight;
-//                 previous[v] = u;  
-//                 minHeap.push({distances[v], v});
-//             }
-//         }
-//     }
-//     return distances;
-// }
-
-
-// vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
-//     vector<int> path;
-    
-//     // If destination is unreachable, return "No path found"
-//     if (distances[destination] == INF || previous[destination] == -1) {
-//         return path;
-//     }
-
-//     // Traverse back using `previous` to reconstruct the path
-//     for (int v = destination; v != -1; v = previous[v]) {
-//         path.push_back(v);
-//     }
-//     reverse(path.begin(), path.end()); // Reverse to get correct order
-//     return path;
-// }
 
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
     vector<int> path;
     
-    // If destination is unreachable, return "No path found"
+    // if unreachable ->"No path found"
     if (distances[destination] == INF || previous[destination] == -1) {
         return path;
     }
 
-    // Traverse back using `previous` to reconstruct the path
+    // need to go back to the previous to get the new path
     for (int v = destination; v != -1; v = previous[v]) {
         path.push_back(v);
     }
-    reverse(path.begin(), path.end()); // Reverse to get correct order
+    // need to reverse to get the correct order
+    reverse(path.begin(), path.end()); 
     return path;
 }
 
@@ -143,9 +83,9 @@ void print_path(const vector<int>& path, int total_cost) {
     }
 
     for (size_t i = 0; i < path.size(); i++) {
+        if (i > 0) cout << " ";  
         cout << path[i];
-        if (i != path.size() - 1) cout << " -> ";
     }
 
-    cout << "\nTotal cost is " << total_cost << endl;
+    cout << " \nTotal cost is " << total_cost << endl;
 }
